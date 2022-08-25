bf5d19fb5b106d5438b9f29876ff91f9957a009f
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-08-25 17:52:28 +0100

    Add React project with authentication

---
+++ Northwind.React.UI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using Northwind.React.UI.Models;
+
+namespace Northwind.React.UI.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions)
+    {
+
+    }
+}
---
+++ Northwind.React.UI/Models/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Northwind.React.UI.Models;
+
+public class ApplicationUser : IdentityUser
+{
+}
