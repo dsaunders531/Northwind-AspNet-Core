621c54ff773df0ca107831d4ffff730018609f5b
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-08-26 14:19:20 +0100

    Update to .Net 6

---
+++ tools.essentials/Services/IAppConfigurationService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+
+namespace tools.Services
+{
+    public interface IAppConfigurationService
+    {
+        IConfigurationRoot Configuration { get; }
+        IFileProvider WebRootFileProvider { get; }
+        string WebRootPath { get; }
+        bool IsDevelopment { get; }
+        bool IsStaging { get; }
+        bool IsProduction { get; }
+    }
+}
