c1643e18ad21437944605169552497249e6aa186
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-08-29 12:47:09 +0100

    Increase perfomance of getting oidc details

--- Northwind.React.UI/Controllers/OidcConfigurationController.cs
+++ Northwind.React.UI/Controllers/OidcConfigurationController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+using System.Collections.Concurrent;
 
 namespace Northwind.React.UI.Controllers;
 
@@ -13,14 +16,56 @@ public class OidcConfigurationController : Controller
     {
         ClientRequestParametersProvider = clientRequestParametersProvider;
         _logger = logger;
+
+        if (OidcConfigurationController.ClientParameters == default 
+            || (OidcConfigurationController.DictionaryRefreshedLast ?? DateTime.UtcNow.AddHours(-2)) < DateTime.UtcNow.AddHours(-1) )
+        {
+            OidcConfigurationController.ClientParameters = new ConcurrentDictionary<string, IDictionary<string, string>>();
+            OidcConfigurationController.DictionaryRefreshedLast = DateTime.UtcNow;
+        }
     }
 
     public IClientRequestParametersProvider ClientRequestParametersProvider { get; }
 
+    // These things are needed for speed. Calling database each time is slow.
+    private static ConcurrentDictionary<string, IDictionary<string, string>> ClientParameters { get; set; }
+
+    private static object DictionaryLock = new object();
+
+    private static DateTime? DictionaryRefreshedLast { get;set;}
+
     [HttpGet("_configuration/{clientId}")]
     public IActionResult GetClientRequestParameters([FromRoute] string clientId)
     {
-        var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
-        return Ok(parameters);
+        try
+        {
+            IDictionary<string, string> result;
+
+            if (OidcConfigurationController.ClientParameters.TryGetValue(clientId, out result))
+            {                
+            }
+            else
+            {
+                result = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
+
+                if (Monitor.TryEnter(OidcConfigurationController.DictionaryLock,TimeSpan.FromSeconds(3)))
+                {
+                    _ = OidcConfigurationController.ClientParameters.TryAdd(clientId, result);
+
+                    Monitor.Exit(OidcConfigurationController.DictionaryLock);
+                }
+                else
+                {
+                    // does not matter - we have the result object.
+                }                
+            }
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            this._logger.LogError($"Error getting parameters for '{clientId}'", ex);
+            throw;
+        }        
     }
 }
