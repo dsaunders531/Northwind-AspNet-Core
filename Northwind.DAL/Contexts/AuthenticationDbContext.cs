3e5e4d10954ecad20769fb7870fb71b9a5d9a9f1
dsaunders531 <duncan.saunders@pobroadband.co.uk>
2022-11-28 11:27:15 +0000

    Update database schema

--- Northwind.DAL/Contexts/AuthenticationDbContext.cs
+++ Northwind.DAL/Contexts/AuthenticationDbContext.cs
@@ -19,6 +19,16 @@ namespace Northwind.DAL
             // Nothing to add yet...
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Identity");          
+        }
+
         /// <summary>
         /// Create default roles on application startup.
         /// </summary>
